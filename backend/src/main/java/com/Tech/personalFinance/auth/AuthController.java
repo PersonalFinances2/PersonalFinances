dc9f8f7c6a246eb1c93c0c0c3dab7e35c8ec6cf9
victor49 <vl491249@gmail.com>
2026-06-04 10:58:21 -0500

    Se creo el login

---
+++ backend/src/main/java/com/Tech/personalFinance/auth/AuthController.java
@@ -0,0 +1,27 @@
+package com.Tech.personalFinance.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Tech.personalFinance.domain.dto.LoginRequest;
+import com.Tech.personalFinance.domain.dto.LoginResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
