ac9cbe2ef8bc7dcdb843432312866ec0baa4fcef
Miguel <mcaceresa@unal.edu.co>
2026-06-05 08:43:32 -0500

    Create crud user

--- frontend/personal-finance-frontend/src/App.js
+++ frontend/personal-finance-frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import ResumenFinanciero from './components/movimientos/ResumenFinanciero';
+import UsuarioForm from './components/usuario/UsuarioForm';
 import MetaPage from './pages/MetaPage';
 
 function App() {
-
   return (
     <div>
       <h1>Personal Finance</h1>
       <ResumenFinanciero />
       <MetaPage />
+      <UsuarioForm />
     </div>
   );
 }
